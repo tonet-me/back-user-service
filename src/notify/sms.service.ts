c2cf12f568e8b00e863e6f8a479008e4c386ffed
MasoodKeshvari <keshvari.developer@gmail.com>
2021-11-30 10:00:00 +0330

    fix

--- src/notify/sms.service.ts
+++ src/notify/sms.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SmsOtpPattenDTO } from './dto/otp.pattern.dto';
-import { Kavenegar } from 'ts-kavenegar';
+import { Kavenegar, KavenegarError, LookupDto, SendDTO } from 'ts-kavenegar';
 @Injectable()
 export class SmsService {
   readonly api: Kavenegar;
@@ -14,13 +14,22 @@ export class SmsService {
   ): Promise<boolean | Error> {
     var regex = /\d+/g;
     const receptor = '00' + data.phoneNumber.match(regex); // creates array from matches
-    const result = await this.api.lookup({
+    const sendData: LookupDto = {
       receptor,
       token: data.code,
       template: 'tonet-verify-code',
-    });
+    };
+    return this.api
+      .lookup(sendData)
+      .then((res) => {
+        return true;
+      })
+      .catch((err) => {
+        if (err instanceof KavenegarError) throw err;
+        else throw new Error('notify not work...');
+      });
 
-    if (result.return.status == 200) return true;
-    else throw new Error(`message not send with code ${result.return.status}`);
+    // if (result.return.status == 200) return true;
+    // else throw new Error(`message not send with code ${result.return.status}`);
   }
 }
